165ab1e3f0e2e1138d2f5d54e7cfed763f645b87
Neesar Banna <Bannasqe@gmail.com>
2023-10-19 14:27:57 -0400

    arithmetic lab

---
+++ index.js
@@ -0,0 +1,42 @@
+1 + 80; //=> 81
+60 - 40; //=> 20
+2 * 3.4; //=> 6.8
+5.0 / 2.5; //=> 2
+
+function add(num1 , num2) {
+return num1 + num2
+  }
+  console.log(add(1 + 80));
+
+
+function subtract (num1, num2){
+    return num1 - num2 
+    }
+    console.log(subtract(60 - 40)); 
+
+function multiply (num1, num2){
+    return num1 * num2 
+    }
+    console.log(multiply(2 * 3.4));    
+
+function divide (num1, num2){
+    return num1 / num2 
+    }
+    console.log(divide(5.0 / 2.5));    
+
+function increment(n) {
+        return n + 1;
+    }
+
+function decrement(n) {
+        return n - 1;
+    }
+
+function makeInt(string) {
+        return parseInt(string, 10);
+    }
+
+function preserveDecimal(string) {
+        return parseFloat(string, 2.35343432);
+    }
+      
